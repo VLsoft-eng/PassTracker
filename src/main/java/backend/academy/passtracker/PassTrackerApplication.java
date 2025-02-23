d770b084fede9adeea676dd7c283b41dfb442172
VLsoft-eng <deeeaddrop@gmail.com>
2025-02-23 21:09:33 +0700

    Initial commit

---
+++ src/main/java/backend/academy/passtracker/PassTrackerApplication.java
@@ -0,0 +1,13 @@
+package backend.academy.passtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/academy/passtracker/PassTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.academy.passtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
