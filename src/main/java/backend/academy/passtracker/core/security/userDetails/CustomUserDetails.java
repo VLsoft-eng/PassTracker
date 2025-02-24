6f75f55a312c173bf135bad0ababfa8dc477edeb
VLsoft-eng <deeeaddrop@gmail.com>
2025-02-25 01:52:09 +0700

    добавлены кастомные user details и user details service

---
+++ src/main/java/backend/academy/passtracker/core/security/userDetails/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package backend.academy.passtracker.core.security.userDetails;
+
+import backend.academy.passtracker.core.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
